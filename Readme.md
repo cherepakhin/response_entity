9c003d29e43b31f19e4569e1f4eefe1dd64b76a4
vasi <vasi.che@gmail.com>
2026-02-27 13:35:46 +0500

    Readme.md (Возвращается ResponseEntity<?>. Некрасиво (т.к. __?__), но проблему решает.)

--- Readme.md
+++ Readme.md
@@ -1,6 +1,11 @@
-### Возврат ответа REST сервиса с помощью ResponseEntity
+### Возврат ответа REST сервиса с помощью ResponseEntity<?>
+
+Основное в [SimpleObjRestController.java](https://github.com/cherepakhin/response_entity/blob/main/src/main/java/ru/perm/v/simple/SimpleObjRestController.java)
+Возвращается ResponseEntity<?>. Некрасиво (т.к. __?__), но проблему решает.
+
 
 Тестирование REST с помощью:
 
 1. __@WebMvcTest__ SimpleObjRestControllerMvcTest
 2. __@SpringBootTest__ SimpleObjRestControllerSpringBootTest
+
--- src/test/java/ru/perm/v/simple/SimpleObjRestControllerMvcTest.java
+++ src/test/java/ru/perm/v/simple/SimpleObjRestControllerMvcTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(SimpleObjRestController.class)
@@ -19,7 +20,47 @@ public class SimpleObjRestControllerMvcTest {
                         get("/simple/test_message")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("{\"message\":\"test_message\"}"));
+                .andExpect(content().string("{\"message\":\"test_message\"}"))
+                .andDo(print())
+        ;
+        // print:
+        // MockHttpServletRequest:
+        // HTTP Method = GET
+        // Request URI = /simple/test_message
+        // Parameters = {}
+        // Headers = []
+        // Body = <no character encoding set>
+        // Session Attrs = {}
+
+        // Handler:
+        // Type = ru.perm.v.simple.SimpleObjRestController
+        // Method = ru.perm.v.simple.SimpleObjRestController#echo(String)
+
+        // Async:
+        // Async started = false
+        // Async result = null
+
+        // Resolved Exception:
+        // Type = null
+
+        // ModelAndView:
+        // View name = null
+        // View = null
+        // Model = null
+
+        // FlashMap:
+        // Attributes = null
+
+        // MockHttpServletResponse:
+        // Status = 200
+        // Error message = null
+        // Headers = [Content-Type:"application/json"]
+        // Content type = application/json
+        // Body = {"message":"test_message"}
+        // Forwarded URL = null
+        // Redirected URL = null
+        // Cookies = []
+
     }
 
     @Test
